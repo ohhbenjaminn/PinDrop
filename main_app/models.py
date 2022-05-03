3260957c30d793a391492ed0feebf3f56992ea9c
Benjamin <benjaminyhong@domain.com>
2022-05-02 17:03:46 -0700

    create model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+# from django.contrib.auth.models import User
+# Create your models here
+class User(models.Model):
+    name = models.CharField(max_length=100)
 
-# Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=101)
+    location = models.CharField(max_length=300)
+    event_time = models.DateTimeField()
+    time_created = models.DateTimeField()
+    details = models.CharField(max_length=1000)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
