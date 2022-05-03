5c1a65c02e6001f11b40988028fa17d0efce0548
Benjamin <benjaminyhong@domain.com>
2022-05-03 09:35:01 -0700

    create form plus date time switch

--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView
+from .models import Event
 # Create your views here.
 from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
 
 def about(request):
-  return render(request, 'about.html')
+    return render(request, 'about.html')
 
 def events_index(request):
-  return render(request, 'events/index.html', { 'events': events })
+    events = Event.objects.all()
+    return render(request, 'events/index.html', { 'events': events })
+
+def events_detail(request, event_id):
+    event = Event.objects.get(id=event_id)
+    return render(request, 'events/detail.html', { 'event': event })
+
+class EventCreate(CreateView):
+    model = Event
+    fields = ['name', 'location', 'event_time', 'time_created', 'details', 'user']
+#   '__all__'
+
