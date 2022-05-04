8d2c605dc844d962ab7ca38b2a15d6052b562edf
shaybay <pacificconnect310@gmail.com>
2022-05-04 12:13:17 -0700

    log out redirect to events page

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,9 +1,15 @@
 from django.forms import ModelForm
-
+from django import forms
 from .models import Event
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
 class EventForm(ModelForm):
     class Meta:
         model = Event
         fields = ['user', 'lat', 'lng']
+        widgets = {
+            'event_time': DateInput(),
+            'time_created': DateInput(),
+        }
