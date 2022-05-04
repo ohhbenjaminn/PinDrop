b3550ad6efd2bf18403f226d7f7f6832e234f939
shaybay <pacificconnect310@gmail.com>
2022-05-03 22:31:07 -0700

    implement login UI

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import date
+
 # Create your models here
 # class User(models.Model):
 #     name = models.CharField(max_length=100)
@@ -13,7 +14,7 @@ class Event(models.Model):
     time_created = models.DateTimeField()
     details = models.CharField(max_length=1000)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
+    
     def __str__(self):
         return self.name
 
