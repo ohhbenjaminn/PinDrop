04d97f330437ec62952a32cd081e8bc86efeb125
Benjamin <benjaminyhong@domain.com>
2022-05-04 16:59:03 -0700

    fix time date picker

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
 from django import forms
 from .models import Event
-from .widgets import DateTimePickerInput
+from .widgets import DateTimePickerInput, DatePickerInput, TimePickerInput
 
 class DateInput(forms.DateInput):
     input_type = 'date'
@@ -10,5 +10,13 @@ class EventForm(ModelForm):
     class Meta:
         model = Event
         fields = ['user', 'lat', 'lng']
+        # , 'event_time_field', 'event_date_field'
         event_time = forms.DateTimeField(widget=DateTimePickerInput)
-        time_created = forms.DateTimeField(widget=DateTimePickerInput)
+        event_date = forms.DateTimeField(widget=DateTimePickerInput)
+
+        # widgets = {
+        #     'event_time_field' : TimePickerInput(),
+        #     'event_date_field'  : DatePickerInput(),
+        # }
+
+        
