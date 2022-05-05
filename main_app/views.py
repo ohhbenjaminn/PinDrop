03976005b5c11690d2db58c3f830548d24bf4733
Benjamin <benjaminyhong@domain.com>
2022-05-04 22:03:07 -0700

    login redirect

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.forms import DateTimeField, DateTimeInput
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core import serializers
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Event
