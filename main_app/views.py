7deea5245044ad3bba6e152d0563fc42bab663f0
Benjamin <benjaminyhong@domain.com>
2022-05-06 12:24:28 -0700

    removed commented out texts and unnecessary comments

--- main_app/views.py
+++ main_app/views.py
@@ -5,16 +5,13 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView, ModelF
 from .models import Event
 from .forms import EventForm
 import json
-# Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
-
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Define the home view
+
 def home(request):
     events = Event.objects.all()
     return render(request, 'events/index.html', { 'events': events })
@@ -33,9 +30,7 @@ def events_detail(request, event_id):
 
 class EventCreate(LoginRequiredMixin, CreateView):
     model = Event
-    # form = EventForm
     fields = ['name', 'location', 'event_time', 'event_date', 'details', 'event_type']
-#   '__all__'
     success_url = '/events/'
 
    
@@ -46,18 +41,14 @@ class EventCreate(LoginRequiredMixin, CreateView):
         event.user = self.request.user
         event.lat = self.request.GET.get('lat', '')
         event.lng = self.request.GET.get('lng', '')
-        # event.lng = json_stuff.lng
-        #article.save()  # This is redundant, see comments.
         return super(EventCreate, self).form_valid(form)
     
 
 class EventUpdate(UpdateView):
   model = Event
-#   fields = ['name', 'location', 'event_time', 'event_date', 'details', 'event_type']
 
   form_class = EventForm
 
-  #   '__all__'
   def form_valid(self, form):
         event = form.save(commit=False)
         event.user = self.request.user 
@@ -67,17 +58,9 @@ class EventDelete(LoginRequiredMixin, DeleteView):
   model = Event
   success_url = '/events/'
 
-# def event_to_JSON(event):
-#     dict = {}
-#     dict[""]
-
 def get_JSON(request):
     events_JSON = Event.objects.all().values('id', 'user', 'name', 'location', 'event_time', 'event_date', 'details', 'lat', 'lng', 'event_type')
     return HttpResponse(events_JSON)
-
-# def create_JSON(request):
-#     pass
-
 
 def signup(request):
     error_message = ""
